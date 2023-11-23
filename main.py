5e601f6f48ba5cc9db71e419cc5d6b69b32292ae
Nick Lockhart <nick.lockhart@fullfibre.co>
2023-11-23 12:29:05 +0000

    New directory-based approach

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import requests
 import yaml
 import os
 import sys
+import re
 
 LNMS_HOST = os.environ.get("LNMS_HOST")
 if not LNMS_HOST:
@@ -39,6 +40,7 @@ def represent_none(self, _):
 
 yaml.add_representer(type(None), represent_none)
 
+# Create file with just devices
 devices_resp = SESSION.get(f"{API_ROOT}/devices")
 if devices_resp.status_code != 200:
     error = devices_resp.json()["message"]
@@ -47,62 +49,99 @@ if devices_resp.status_code != 200:
 
 device_data = devices_resp.json()["devices"]
 
-groups_data = SESSION.get(f"{API_ROOT}/devicegroups").json()
+all_devices = {"ungrouped": {"hosts": dict()}}
+names_by_id = dict()
 
-print(f"Processing {groups_data['count']} groups...")
+print(f"Processing {len(device_data)} devices... ", end='')
+for device in device_data: 
+    if not device["sysName"]: # our Libre instance has a device with an empty name
+        continue
 
-grouped_ids = set()
-
-for group in groups_data["groups"]:
-    group_id = group["id"]
-
-    # Clean up group name to fit Ansible standards
-    group_name: str = group["name"]
-    group_name = group_name.replace(" ", "_").replace("-", "_")
-
-    print(f"Processing group {group_name}...", end='\r')
-
-    inventory[group_name] = dict()
-    inventory[group_name]["hosts"] = dict()
-
-    group_data = SESSION.get(f"{API_ROOT}/devicegroups/{group_id}").json()
-
-    devices = [device["device_id"] for device in group_data["devices"]]
-    grouped_ids.update(devices) # add these to "grouped_ids" so they're not put in unsorted
-
-    for device in devices:
-        group_device_data = SESSION.get(f"{API_ROOT}/devices/{device}").json()["devices"][0]
-        device_host = group_device_data["hostname"]
-        device_name = group_device_data["sysName"]
-
-        if device_name is None:
-            # see later in the code - someone put an unnamed devices in our Libre
-            continue    
-
-        # Clean up device name for Ansible
-        # Shouldn't be as nessecary as for group names, but won't hurt anything
-        device_name = device_name.replace(" ", "_").replace("-", "_")
-
-        ansible_device = {
-            "ansible_host": device_host
-        }
-
-        inventory[group_name]["hosts"][device_name] = ansible_device
-    
-    print(f"Processing group {group_name}... done")
-
-ungrouped_devices = [device for device in device_data if device["device_id"] not in grouped_ids]
-
-for device in ungrouped_devices:
     ansible_device = {
         "ansible_host": device["hostname"]
     }
-    device_name = device["sysName"]
-    if not device_name:
-        # dangit, who put an empty device name into Libre?
+    device_id = device["device_id"]
+
+    device_name: str = device["sysName"]
+    # Clean up hostname for Ansible
+    device_name = device_name.lower()
+    device_name = re.sub(r'[\W_]+', '_', device_name)
+
+    if not device_name: # see above - safeguard
         continue
     else:
-        inventory["ungrouped"]["hosts"][device_name] = ansible_device
+        all_devices["ungrouped"]["hosts"][device_name] = ansible_device
+        names_by_id[device_id] = device_name
 
-with open("inventory.yaml", "w") as inventory_file:
-    yaml.dump(data=inventory, stream=inventory_file, default_flow_style=False)
+print("done.")
+
+print(f"Writing device data to file... ", end='')
+with open("inventory/00-hosts.yaml", "w") as hosts_file:
+    yaml.dump(data=all_devices, stream=hosts_file, default_flow_style=False)
+print("done.")
+
+# Create a file with group definitions
+all_groups = dict()
+
+groups_data = SESSION.get(f"{API_ROOT}/devicegroups").json()["groups"]
+
+print(f"Processing {len(groups_data)} groups...")
+for group in groups_data:
+    group_id = group["id"]
+    group_name: str = group["name"]
+
+    # Sanitise group name for Ansible
+    group_name = group_name.lower()
+    group_name = re.sub(r'[\W_]+', '_', group_name)
+
+    all_groups[group_name] = {"hosts": dict()}
+
+    print(f"Processing group {group_name}... ", end='')
+    group_data = SESSION.get(f"{API_ROOT}/devicegroups/{group_id}").json()
+    for device in group_data["devices"]:
+        device_id = device["device_id"]
+        if device_id in names_by_id:
+            group_device_name = names_by_id[device_id]
+            # Device already defined in 00-hosts, no need to put anything here
+            # other than specify its existence
+            all_groups[group_name]["hosts"][group_device_name] = None
+
+    print("done.")
+
+print("Finished processing groups.")
+
+print("Writing group data to file... ", end='')
+with open("inventory/01-groups.yaml", "w") as groups_file:
+    yaml.dump(data=all_groups, stream=groups_file, default_flow_style=False)
+print("done.")
+
+# Create a file with location definitions
+
+all_locations = dict()
+
+print("Creating locations data... ", end='')
+for device in device_data:
+    device_id = device["device_id"]
+    if device_id not in names_by_id:
+        continue
+    else:
+        device_name = names_by_id[device_id] # presanitised, let's not stress regex
+
+    device_location: str = device["location"]
+    # You know the deal by now.
+    device_location = device_location.lower()
+    device_location = re.sub(r'[\W_]+', '_', device_location)
+
+    if device_location not in all_locations.keys():
+        all_locations[device_location] = {"hosts": dict()}
+    
+    # see earlier in the code - host already defined
+    # just need to say it's in the group
+    all_locations[device_location]["hosts"][device_name] = None
+
+print("done.")
+
+print("Writing locations data to file... ", end='')
+with open("inventory/02-locations.yaml", "w") as locations_file:
+    yaml.dump(data=all_locations, stream=locations_file, default_flow_style=False)
+print("done.")
